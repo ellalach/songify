83d30804051abb265f346f3dcc6c6a54e37c564a
Ella Lachevet <ellalachevet@gmail.com>
2026-01-05 13:09:23 -0500

    Expand artist dictionary with new genres and artists
    Updated the artist dictionary to include additional genres and artists.

--- artist_dictionary.py
+++ artist_dictionary.py
@@ -1,11 +1,111 @@
-{"pop": ["Bruno Mars", "The Weeknd", "Taylor Swift", "Billie Ellish", "Ariana Grande", "Lady Gaga", "Rihanna", "Dua Lipa", "Beyoncé", "Olivia Rodrigo"],
-"rap": ["Drake", "Travis Scott", "Kanye West", "21 Savage", "Future", "Metro Boomin", "Kendrick Lamar", "Childish Gambino", "Playboi Carti", "Mac Miller"],
-"rock": ["Guns N' Roses", "Red Hot Chili Peppers", "Billy Joel", "Foo Fighters", "AC/DC", "Radiohead", "Queen", "Bon Jovi", "Fleetwood Mac", "Radio Head"],
-"hip hop": ["Future", "Lil Wayne", "21 Savage", "JAY-Z", "Kanye West", "Tyler The Creator", "J-Cole", "Nicki Minaj", "A$AP Rocky", "2Pac"], 
-"dance pop": ["Bruno Mars", "Dua Lipa", "Lady Gaga", "Charli XCX", "Britney Spears", "Pitbull", "Black Eyed Peas", "Pharrel Williams", "Destiny's Child"], 
-"pop rap":["Drake", "Eminem", "Post Malone", "Travis Scott", "Don Toliver", "Future", "Tyler The Creator", "Metro Boomin", "21 Savage", "Nicki Minaj"], 
-"pov: indie":["Laufey", "Hozier", "Noah Kahan", "Mitski", "TV Girl", "Lana Del Rey", "Dominic Fike", "Glass Animals", "Phoebe Bridgers", "Alex G"], 
-"art pop":["Lady Gaga", "Billie Eilish", "Lana Del Rey", "Charli XCX", "Lorde", "Grimes", "Fiona Apple", "AURORA", "Kate Bush", "Suki Waterhouse"], 
-"jazz":["Frank Sinatra", "Ella Fitzgerald", "Louis Armstrong", "Tony Bennet", "Nina Simone", "Peggy Lee", "Stan Getz", "Jamie Cullum", "Billie Holiday", "Count Basie"], 
-"r&b":["SZA", "Beyoncé", "Chris Brown", "Michael Jackson", "USHER", "Alicia Keys", "Miguel", "Brent Faiyaz", "Mary J. Blige", "H.E.R"], 
-"country":["Morgan Wallen", "Zach Bryan", "Luke Combs", "Dolly Parton", "Christ Stapleton", "Shania Twain", "Carrie Underwood", "Luke Bryan", "Tyler Childers", "Tim McGraw"]}
+{
+    "pop": [
+        "Bruno Mars", "The Weeknd", "Taylor Swift", "Billie Eilish",
+        "Ariana Grande", "Lady Gaga", "Rihanna", "Dua Lipa",
+        "Beyoncé", "Olivia Rodrigo"
+    ],
+
+    "rap": [
+        "Drake", "Travis Scott", "Kanye West", "21 Savage",
+        "Future", "Metro Boomin", "Kendrick Lamar",
+        "Childish Gambino", "Playboi Carti", "Mac Miller"
+    ],
+
+    "hip hop": [
+        "JAY-Z", "Lil Wayne", "Nas", "J. Cole",
+        "Tyler, The Creator", "Nicki Minaj",
+        "A$AP Rocky", "2Pac", "Biggie Smalls", "Ice Cube"
+    ],
+
+    "rock": [
+        "Guns N' Roses", "Red Hot Chili Peppers", "Billy Joel",
+        "Foo Fighters", "AC/DC", "Radiohead", "Queen",
+        "Bon Jovi", "Fleetwood Mac", "Nirvana"
+    ],
+
+    "alternative rock": [
+        "Arctic Monkeys", "The Strokes", "Tame Impala",
+        "Cage The Elephant", "The Killers",
+        "Muse", "Interpol", "The Smashing Pumpkins",
+        "The Black Keys", "Yeah Yeah Yeahs"
+    ],
+
+    "dance pop": [
+        "Dua Lipa", "Lady Gaga", "Charli XCX",
+        "Britney Spears", "Pitbull", "Black Eyed Peas",
+        "Pharrell Williams", "Calvin Harris",
+        "Kesha", "Ava Max"
+    ],
+
+    "pop rap": [
+        "Drake", "Eminem", "Post Malone",
+        "Don Toliver", "Jack Harlow",
+        "Tyler, The Creator", "Nicki Minaj",
+        "Lil Nas X", "Doja Cat", "Juice WRLD"
+    ],
+
+    "pov: indie": [
+        "Laufey", "Hozier", "Noah Kahan", "Mitski",
+        "TV Girl", "Lana Del Rey", "Dominic Fike",
+        "Glass Animals", "Phoebe Bridgers", "Alex G"
+    ],
+
+    "indie pop": [
+        "Clairo", "Boygenius", "Wallows",
+        "Rex Orange County", "Beabadoobee",
+        "Faye Webster", "Dayglow",
+        "Still Woozy", "Men I Trust", "Japanese Breakfast"
+    ],
+
+    "art pop": [
+        "Lady Gaga", "Billie Eilish", "Lana Del Rey",
+        "Charli XCX", "Lorde", "Grimes",
+        "Fiona Apple", "AURORA", "Kate Bush", "Caroline Polachek"
+    ],
+
+    "jazz": [
+        "Frank Sinatra", "Ella Fitzgerald", "Louis Armstrong",
+        "Tony Bennett", "Nina Simone", "Peggy Lee",
+        "Stan Getz", "Billie Holiday", "Count Basie", "Chet Baker"
+    ],
+
+    "r&b": [
+        "SZA", "Beyoncé", "Usher", "Alicia Keys",
+        "Miguel", "Brent Faiyaz", "Mary J. Blige",
+        "H.E.R.", "Frank Ocean", "Summer Walker"
+    ],
+
+    "neo soul": [
+        "Erykah Badu", "D'Angelo", "Lauryn Hill",
+        "Jill Scott", "Anderson .Paak",
+        "Solange", "SiR", "Daniel Caesar",
+        "Ari Lennox", "Musiq Soulchild"
+    ],
+
+    "country": [
+        "Morgan Wallen", "Zach Bryan", "Luke Combs",
+        "Dolly Parton", "Chris Stapleton", "Shania Twain",
+        "Carrie Underwood", "Luke Bryan",
+        "Tyler Childers", "Tim McGraw"
+    ],
+
+    "alt country / folk": [
+        "Kacey Musgraves", "Bon Iver", "The Lumineers",
+        "Mumford & Sons", "Caamp",
+        "The Avett Brothers", "Fleet Foxes",
+        "Phoebe Bridgers", "Jason Isbell", "Gregory Alan Isakov"
+    ],
+
+    "edm": [
+        "Calvin Harris", "Avicii", "Martin Garrix",
+        "Zedd", "Illenium", "Deadmau5",
+        "Skrillex", "Kygo", "Marshmello", "ODESZA"
+    ],
+
+    "lo-fi / chill": [
+        "Joji", "Tycho", "Nujabes",
+        "Clairo", "Rex Orange County",
+        "Cuco", "Still Woozy",
+        "Girl in Red", "beabadoobee", "Khruangbin"
+    ]
+}
